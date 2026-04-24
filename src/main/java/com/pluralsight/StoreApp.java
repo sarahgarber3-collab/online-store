02aac3d641b8366eef401d4460b475262f961803
sarahgarber3@gmail.com <sarahgarber3@gmail.com>
2026-04-24 10:00:42 -0400

    updates to the code and added homescreen. Also added a switch statement for the different options for the user input

--- src/main/java/com/pluralsight/StoreApp.java
+++ src/main/java/com/pluralsight/StoreApp.java
@@ -5,14 +5,42 @@ import java.util.Scanner;
 public class StoreApp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Welcome to the online store!");
-        System.out.println("Select from the following options: ");
-        System.out.println("\t1) Display Products");
-        System.out.println("\t2) Display Cart");
-        System.out.println("\t0) Exit");
-        System.out.print("Enter your selection: ");
-        int userOption = scanner.nextInt();
 
-
+        homeScreen(scanner);
     }
-}
+    public static void homeScreen(Scanner scanner) {
+
+        while (true) {
+            System.out.println("Select from the following options: ");
+            System.out.println("\t1) Display Products");
+            System.out.println("\t2) Display Cart");
+            System.out.println("\t0) Exit");
+            System.out.print("Enter your selection: ");
+            int userOption = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (userOption) {
+                case 1:
+                    //               displayProducts();
+                    System.out.println("\n\n");
+                    break;
+                case 2:
+                    //               displayCart();
+                    System.out.println("\n\n");
+                    break;
+                case 0:
+                    System.out.println("Thanks for visiting my online store!!");
+                    scanner.close();
+                    System.exit(0);
+                default:
+                    System.out.println("Incorrect option entered");
+                    scanner.nextLine();
+                    System.out.println("\n\n");
+            }
+
+
+        }
+    }
+    }
