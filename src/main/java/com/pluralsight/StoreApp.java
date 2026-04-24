0c6ef537d62dec4bc8891bc0073fe04a96ad512f
sarahgarber3@gmail.com <sarahgarber3@gmail.com>
2026-04-24 09:26:17 -0400

    Scanner added and inputs

--- src/main/java/com/pluralsight/StoreApp.java
+++ src/main/java/com/pluralsight/StoreApp.java
@@ -1,4 +1,18 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class StoreApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the online store!");
+        System.out.println("Select from the following options: ");
+        System.out.println("\t1) Display Products");
+        System.out.println("\t2) Display Cart");
+        System.out.println("\t0) Exit");
+        System.out.print("Enter your selection: ");
+        int userOption = scanner.nextInt();
+
+
+    }
 }
